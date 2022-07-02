0370e8d0165b3961213399680b8ad112ff8973d8
Jeremy Hage <jeremy.hage@dialexa.com>
2022-07-01 20:29:12 -0500

    extracted greetingPrefix; added named return

--- hello_test.go
+++ hello_test.go
@@ -10,14 +10,26 @@ func TestHello(t *testing.T) {
 		}
 	}
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Jeremy")
+		got := Hello("Jeremy", "")
 		want := "Hello, Jeremy"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is provided", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Albs", "Spanish")
+		want := "Hola, Albs"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Logn", "French")
+		want := "Bonjour, Logn"
 		assertCorrectMessage(t, got, want)
 	})
 }
